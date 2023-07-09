caaa3b354e8f0123173078b1ea2a6ea9c5b05e6a
minaripenguin <minaripenguin@users.noreply.github.com>
2023-07-09 16:56:10 +0800

    parallel space: Update blocklist
    Change-Id: I337ad3dca01f5f9a0ce053e011c38d5c12925c4b
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- services/core/java/ink/kaleidoscope/server/ParallelSpaceManagerService.java
+++ services/core/java/ink/kaleidoscope/server/ParallelSpaceManagerService.java
@@ -97,11 +97,14 @@ public final class ParallelSpaceManagerService extends SystemService {
     private static final List<String> SPACE_BLOCKLIST_PACKAGES = Arrays.asList(
         // To avoid third party apps starting it accidentally.
         "com.android.launcher3",
+        "com.android.wallpaper",
+        "com.google.android.wallpaper",
         "com.android.server.telecom",
         "com.google.android.apps.nexuslauncher",
+        "com.google.android.pixel.setupwizard",
         "com.google.android.projection.gearhead",
         "org.lineageos.setupwizard",
-        "com.crdroid.updater"
+        "org.risingos.updater"
     );
 
     /**
