Albinoman887/frameworks_base

SystemUI: Media notification artwork fade and blur filter [1/2]
Co-authored-by: jhonboy121 <alfredmathew05@gmail.com>
Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -89,6 +89,8 @@ public class SystemSettings {
         Settings.System.NOTIFICATION_LIGHT_PULSE,
         Settings.System.NETWORK_TRAFFIC_STATE,
         Settings.System.NETWORK_TRAFFIC_AUTOHIDE_THRESHOLD,
-        Settings.System.QS_PANEL_STYLE
+        Settings.System.QS_PANEL_STYLE,
+        Settings.System.MEDIA_ARTWORK_BLUR_ENABLED,
+        Settings.System.MEDIA_ARTWORK_BLUR_RADIUS
     };
 }

SENSEI-OS/android_frameworks_base

Change-Id: I5d22ab30431fbfbc2def659fb62c4fdd958d0bfb

packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -93,6 +93,8 @@ public class SystemSettings {
         Settings.System.QS_TILE_LABEL_HIDE,
         Settings.System.QS_TILE_VERTICAL_LAYOUT,
         Settings.System.QS_LAYOUT,
-        Settings.System.QQS_LAYOUT
+        Settings.System.QQS_LAYOUT,
+        Settings.System.MEDIA_ARTWORK_BLUR_ENABLED,
+        Settings.System.MEDIA_ARTWORK_BLUR_RADIUS
     };
 }